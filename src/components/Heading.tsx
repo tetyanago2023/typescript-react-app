a98c2e88797704ccafb1a8c64287a492abb8cd40
Tetyana2015 <tgoncharenko2013@gmail.com>
2024-07-15 15:14:53 -0700

    Initial commit

---
+++ src/components/Heading.tsx
@@ -0,0 +1,10 @@
+import {ReactElement} from "react";
+
+type HeadingProps = { title: string }
+const Heading = ( {title}: HeadingProps): ReactElement => {
+    return (
+        <h1>{title}</h1>
+    );
+};
+
+export default Heading;
